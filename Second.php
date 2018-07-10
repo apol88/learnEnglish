cb5d0112b81f36bd2c824d8d177abf759cbef465
Andrey <sh88andrey@gmail.com>
2018-07-10 18:25:54 +0400

    zzzz

---
+++ Second.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrey
+ * Date: 10.07.2018
+ * Time: 18:24
+ */
+
+namespace apol88\learnenglish;
+
+
+class Second
+{
+    /*
+     * run
+     */
+    public function run()
+    {
+        echo 'Hi';
+        echo "<br>";
+        echo 'Hi2';
+        echo "<br>";
+        echo 'Hi3';
+    }
+
+}
