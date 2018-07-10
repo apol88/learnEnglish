19639196a44459e9cf62329f04e38181eab3cc7c
Andrey <sh88andrey@gmail.com>
2018-07-10 16:28:15 +0400

    zzzz

---
+++ First.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrey
+ * Date: 10.07.2018
+ * Time: 16:27
+ */
+
+namespace learnenglish;
+
+
+class First
+{
+    /*
+     * run
+     */
+    public function run()
+    {
+        echo 'Hi';
+    }
+}
